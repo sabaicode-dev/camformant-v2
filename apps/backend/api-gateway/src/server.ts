3e8b44964c5ec400c7b3de933ae69393bf6376f6
MyDragon168 <cambodiagames3@gmail.com>
2024-10-30 23:07:38 +0700

    CI test

--- apps/backend/api-gateway/src/server.ts
+++ apps/backend/api-gateway/src/server.ts
@@ -1,15 +1,15 @@
 import configs from "@/src/config";
 
-import app from "@/src/app"
-import createLogger from "@/src/utils/logger";
+import app from "@/src/app";
+// import createLogger from "@/src/utils/logger";
 
-export const gatewayLogger = createLogger({ service: 'api-gateway', level: 'info', logGroupName: configs.awsCloudwatchLogsGroupName });
+// export const gatewayLogger = createLogger({ service: 'api-gateway', level: 'info', logGroupName: configs.awsCloudwatchLogsGroupName });
 
 async function run() {
   try {
     app.listen(configs.port, () => {
-      console.log(`Gateway Service running on Port:`, configs.port)
-    })
+      console.log(`Gateway Service running on Port:`, configs.port);
+    });
   } catch (error) {
     console.error("Failed to start the application:", error);
     process.exit(1);
