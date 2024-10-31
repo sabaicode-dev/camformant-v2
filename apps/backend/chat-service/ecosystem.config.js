58eb76c5cb3f4da3e8fcb2cd7259df63fb25ecfd
MyDragon168 <cambodiagames3@gmail.com>
2024-10-31 21:59:58 +0700

    on fix running EC2

--- apps/backend/chat-service/ecosystem.config.js
+++ apps/backend/chat-service/ecosystem.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-    apps: [{
+  apps: [
+    {
       name: "chat-service",
-      script: "server.js",
+      script: "./server.js",
       instances: 1,
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
-    }]
-  };
-  
+      max_memory_restart: "1G",
+    },
+  ],
+};
