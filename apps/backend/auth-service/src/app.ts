2d3b1ee9489321d9efbec414c9b79ad47f1e3d44
MyDragon168 <cambodiagames3@gmail.com>
2024-10-31 14:05:13 +0700

    complete yml CICD

--- apps/backend/auth-service/src/app.ts
+++ apps/backend/auth-service/src/app.ts
@@ -1,47 +1,44 @@
-import express from 'express';
+import express from "express";
 import swaggerUi from "swagger-ui-express";
-import { RegisterRoutes } from '@/src/routes/v1/routes';
-import fs from 'fs';
-import path from 'path'
-import { globalErrorHandler } from '@/src/middlewares/global-error';
-import cookieParser from 'cookie-parser';
+import { RegisterRoutes } from "@/src/routes/v1/routes";
+import fs from "fs";
+import path from "path";
+import { globalErrorHandler } from "@/src/middlewares/global-error";
+import cookieParser from "cookie-parser";
 
 // Dynamically load swagger.json
-const swaggerDocument = JSON.parse(fs.readFileSync(path.join(__dirname, 'docs/swagger.json'), 'utf8'));
-
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "docs/swagger.json"), "utf8")
+);
+//
 // ========================
 // Initialize App Express
 // ========================
 const app = express();
 
-
 // =======================
 // Security Middlewares
 // =======================
-app.use(cookieParser())
-
+app.use(cookieParser());
 
 // ========================
 // Commons Middleware
 // ========================
 app.use(express.json());
 
-
 // ========================
 // Global API V1
 // ========================
-RegisterRoutes(app)
-
+RegisterRoutes(app);
 
 // ========================
 // API Documentations
 // ========================
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
 // ========================
 // ERROR Handler
 // ========================
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 export default app;
