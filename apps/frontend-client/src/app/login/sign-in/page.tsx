34a65f1a18f259eee69a81d200d5b1149b0b4ca2
PhorsBeatRmx <phorsbeatrmx0509@gmail.com>
2024-10-31 18:55:28 -1200

    Implement basic authentication features : registration, login, and verification code functionality

--- apps/frontend-client/src/app/login/sign-in/page.tsx
+++ apps/frontend-client/src/app/login/sign-in/page.tsx
@@ -42,6 +42,7 @@ const SignIn = () => {
                     "Content-Type": "application/json",
                 },
                 body: JSON.stringify(values),
+                credentials: "include",
             });
 
             const data = await response.json();
