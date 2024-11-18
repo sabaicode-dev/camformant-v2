652354e555666dc246b4f42d89bd67c8beb61aba
PhorsBeatRmx <phorsbeatrmx0509@gmail.com>
2024-11-17 17:53:43 -1200

    [ backend ] - Modify Auth-service

--- apps/backend/auth-service/src/services/auth.service.ts
+++ apps/backend/auth-service/src/services/auth.service.ts
@@ -705,9 +705,7 @@ class AuthService {
         sub: userInfo.Username,
         email: body.email,
         username: userInfo.UserAttributes?.find((attr) => attr.Name === "name")
-          ?.Value,
-        corporateProfileId: "",
-        role,
+          ?.Value
       });
     } catch (error) {
       console.error("AuthService corporateVerifyUser() method error:", error);
