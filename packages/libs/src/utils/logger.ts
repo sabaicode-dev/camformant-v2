d3cce5d9c0e6acf041753f6a0d7845904d670c12
leakna <phornleakna@gmail.com>
2024-12-05 14:56:28 +0700

    add file size error

--- packages/libs/src/utils/logger.ts
+++ packages/libs/src/utils/logger.ts
@@ -1,13 +1,13 @@
 import { prettyPrint } from '@base2/pretty-print-object';
 
 export default function logger(message: string) {
-  console.log(`message logs: ${message}`)
+  console.log(`message logss: ${message}`);
 }
 
 export function loggerBeautifulObject(data: object) {
-  console.log(prettyPrint(data, { indent: ' ', singleQuotes: false }))
+  console.log(prettyPrint(data, { indent: ' ', singleQuotes: false }));
 }
 
 export function prettyObject(data: {}) {
-  return JSON.stringify(data, null, 2)
+  return JSON.stringify(data, null, 2);
 }
