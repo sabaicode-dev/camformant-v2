83549c23c5f4878ac87349cfcdb1507bde030ce9
kimhab <khunkimhab7@gmail.com>
2024-12-16 09:24:52 +0700

    on CICD

--- apps/camformant-client/sst.config.ts
+++ apps/camformant-client/sst.config.ts
@@ -1,20 +1,33 @@
-import { SSTConfig } from "sst";
-import { NextjsSite } from "sst/constructs";
+/// <reference path="./.sst/platform/config.d.ts" />
 
-export default {
-  config(_input) {
+export default $config({
+  app(input) {
     return {
       name: "camformant-client",
-      region: "us-east-1",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
     };
   },
-  stacks(app) {
-    app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
-
-      stack.addOutputs({
-        SiteUrl: site.url,
-      });
+  async run() {
+    new sst.aws.Nextjs("MyWeb", {
+      environment: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "",
+        NEXT_PUBLIC_AUTH_ENDPOINT: process.env.NEXT_PUBLIC_AUTH_ENDPOINT || "",
+        NEXT_PUBLIC_USER_ENDPOINT: process.env.NEXT_PUBLIC_USER_ENDPOINT || "",
+        NEXT_PUBLIC_JOB_ENDPOINT: process.env.NEXT_PUBLIC_JOB_ENDPOINT || "",
+        NEXT_PUBLIC_COMPANY_ENDPOINT:
+          process.env.NEXT_PUBLIC_COMPANY_ENDPOINT || "",
+        NEXT_PUBLIC_PUSH_NOTIFICATION_ENDPOINT:
+          process.env.NEXT_PUBLIC_PUSH_NOTIFICATION_ENDPOINT || "",
+        NEXT_PUBLIC_VAPID_PUBLIC_KEY:
+          process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || "",
+        NEXT_PUBLIC_CONVERSATION_ENDPOINT:
+          process.env.NEXT_PUBLIC_CONVERSATION_ENDPOINT || "",
+        UPLOADTHING_TOKEN: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || "",
+        NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
+          process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
+      },
     });
   },
-} satisfies SSTConfig;
+});
