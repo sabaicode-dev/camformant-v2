d9bc76ce816650fc2a5b82540669ae76bc70cb6a
kimhab <khunkimhab7@gmail.com>
2024-12-16 09:06:17 +0700

    on CICD

--- apps/camformant-client/sst.config.ts
+++ apps/camformant-client/sst.config.ts
@@ -4,33 +4,13 @@ import { NextjsSite } from "sst/constructs";
 export default {
   config(_input) {
     return {
-      name: "camformant-client-app",
-      region: "ap-southeast-2",
+      name: "camformant-client",
+      region: "us-east-1",
     };
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site", {
-        environment: {
-          NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "",
-          NEXT_PUBLIC_AUTH_ENDPOINT:
-            process.env.NEXT_PUBLIC_AUTH_ENDPOINT || "",
-          NEXT_PUBLIC_USER_ENDPOINT:
-            process.env.NEXT_PUBLIC_USER_ENDPOINT || "",
-          NEXT_PUBLIC_JOB_ENDPOINT: process.env.NEXT_PUBLIC_JOB_ENDPOINT || "",
-          NEXT_PUBLIC_COMPANY_ENDPOINT:
-            process.env.NEXT_PUBLIC_COMPANY_ENDPOINT || "",
-          NEXT_PUBLIC_PUSH_NOTIFICATION_ENDPOINT:
-            process.env.NEXT_PUBLIC_PUSH_NOTIFICATION_ENDPOINT || "",
-          NEXT_PUBLIC_VAPID_PUBLIC_KEY:
-            process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || "",
-          NEXT_PUBLIC_CONVERSATION_ENDPOINT:
-            process.env.NEXT_PUBLIC_CONVERSATION_ENDPOINT || "",
-          UPLOADTHING_TOKEN: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY || "",
-          NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
-            process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
-        },
-      });
+      const site = new NextjsSite(stack, "site");
 
       stack.addOutputs({
         SiteUrl: site.url,
