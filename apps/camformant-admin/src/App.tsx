59e8e54750f9e4470724ad6198e42af23d688b79
leakna <phornleakna@gmail.com>
2024-12-25 10:41:35 +0700

    deploy

--- apps/camformant-admin/src/App.tsx
+++ apps/camformant-admin/src/App.tsx
@@ -9,7 +9,6 @@ import Chart from "./page/Chart";
 import Post from "./page/Post";
 import Camformant from "./page/Dashboard";
 import { AuthProvider } from "./context/authContext";
-// import ProtectedRoutes from "./page/ProtectedRoute";
 import Approval from "./page/Approval";
 import ApprovalDetail from "./page/ApprovalDetail";
 function App() {
